fefbbfc4d2f35cc4c16f1231b342608f1bfe2dd5
Hiba Khan <hiba22khan@gmail.com>
2026-08-25 22:12:37 +0530

    formController and formRoutes created successfully!

--- backend/index.js
+++ backend/index.js
@@ -3,17 +3,20 @@ import cors from "cors";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import connectDB from "./config/db.js";
+import formRoutes from "./routes/formRoutes.js"
 
 dotenv.config();
 
 const app = express();
 
-connectDB();
+
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+
+app.use("/api/forms", formRoutes);
 
 
 app.get("/", (req,res) => {
@@ -24,6 +27,17 @@ app.get("/", (req,res) => {
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-    console.log(`Forma AI server running on port ${PORT}`);
-});
+const startServer = async () => {
+    try {
+        await connectDB();
+
+        app.listen(PORT, () => {
+            console.log(`Forma AI server running on port ${PORT}`);
+        });
+    } catch (error) {
+        console.error("Failed to start server:", error.message);
+        process.exit(1);
+    }
+};
+
+startServer();
